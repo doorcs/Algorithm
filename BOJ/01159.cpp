12fd2e63c859297d2944ea5178a4c85ab12f94b5
성문 <castledoor@pusan.ac.kr>
2024-01-26 18:06:54 +0900

    1159번: 농구 경기

---
+++ BOJ/01159.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+    int rep;
+    cin >> rep;
+
+    map<char, int> mp;
+    for(int i=0; i<rep; i++){
+        string in;
+        cin >> in;
+
+        if(mp.find(in[0]) != mp.end()) mp[in[0]]++;
+        else mp.insert({in[0], 1});
+    }
+
+    string res = "";
+    int flag = 0;
+    for(const auto& [key, val]: mp){
+        if(val >= 5){
+            res += key;
+            flag = 1; // 여러번 실행되도 상관X
+        }
+    }
+
+    if(flag) cout << res;
+    else cout << "PREDAJA";
+}
