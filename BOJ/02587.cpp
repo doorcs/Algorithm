2c08955cae96d57474fec294f3714da0e91edb5c
성문 <castledoor@pusan.ac.kr>
2024-01-28 16:52:00 +0900

    2587번: 대표값2

--- BOJ/02587.cpp
+++ BOJ/02587.cpp
@@ -8,9 +8,7 @@ int main() {
     vector<int> v(5);
 
     int i=5;
-    while(i--){
-        cin >> v[i]; // 어차피 정렬하기 때문에 타자 적게 치는 방식을 시도
-    }
+    while(i--) cin >> v[i]; // 어차피 정렬하기 때문에 타자 적게 치는 방식을 시도
 
     sort(v.begin(), v.end());
     cout << accumulate(v.begin(), v.end(), 0)/5 << endl << v[2] << endl;
