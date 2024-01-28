ba87e4c5f4824b25305adbe7f26e8c53598fe66a
성문 <castledoor@pusan.ac.kr>
2024-01-28 12:14:10 +0900

    2587번: 대표값2

---
+++ BOJ/02587.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric> // accumulate(begin, end, initial value)
+using namespace std;
+
+int main() {
+    vector<int> v(5);
+
+    int i=5;
+    while(i--){
+        cin >> v[i]; // 어차피 정렬하기 때문에 타자 적게 치는 방식을 시도
+    }
+
+    sort(v.begin(), v.end());
+    cout << accumulate(v.begin(), v.end(), 0)/5 << endl << v[2] << endl;
+}
