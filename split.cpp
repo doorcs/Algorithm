59c87d908acd43f47d70b22a7d607b20251ea757
성문 <castledoor@pusan.ac.kr>
2024-03-26 00:39:57 +0900

    vector<string> split(string str, string delim);
    std::string의 메소드들을 활용!
    
    ===============================================================================
    
    # find() 메소드 #
    
    std::size_t std::string::find(const std::string &__str, std::size_t __pos = 0ULL) const noexcept
    
    Find position of a string.
    
    Parameters:
    __str – String to locate.
    __pos – Index of character to search from (default 0).
    
    Returns:
    Index of start of first occurrence. Starting from __pos, searches forward for value of __str within this string.
    If found, returns the index where it begins. If not found, returns npos.
    
    ===============================================================================
    
    # substr() 메소드 #
    
    std::string std::string::substr(std::size_t __pos, std::size_t __n = 18446744073709551615ULL) const
    Get a substring.
    
    Parameters:
    __pos – Index of first character (default 0).
    __n – Number of characters in substring (default remainder).
    
    Returns:
    The new string.
    
    Exceptions:
    std::out_of_range If __pos > size(). Construct and return a new string using the __n characters starting at __pos.
    If the string is too short, use the remainder of the characters. If __pos is beyond the end of the string, out_of_range is thrown.

--- split.cpp
+++ split.cpp
@@ -24,7 +24,7 @@ int main() {
     string str;
     getline(cin, str);
 
-/* std::string의 `find()` 메소드를 활용하기 때문에, char 자료형으로 표현 가능한 delimiter도 double quote로 감싸서 std::string으로 넘겨줘야한다!! */
+/* std::string의 `find()` 메소드를 활용하기 때문에, char 자료형으로 표현 가능한 delimiter도 double quote로 감싸서 std::string으로 넘겨줘야한다!!! */
     auto splitted = split(str, " "); /* delimiter는 변경 가능 */
     cout << splitted.size() << '\n';
 
