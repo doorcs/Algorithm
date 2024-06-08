d75d18e8e9ccc605221ec312e160c38874e91e19
성문 <castledoor@pusan.ac.kr>
2024-06-08 17:52:20 +0900

    5397번: 키로거 ***
    if -> else if -> else 구문에서는
    모호함을 피하기 위해 한줄이어도 블럭으로 감싸주기!!
    
    + std::list의 `.erase()` 메소드 사용시 주의점

--- BOJ/05397.cpp
+++ BOJ/05397.cpp
@@ -9,15 +9,17 @@ int main() {
     int N; cin >> N;
 
     while (N--) {
-        list<char> li;
-        auto curr = li.end();
         string str; cin >> str;
+        list<char> li;
+        auto curr = li.end(); // list<char>::iterator curr = li.end();
 
         for (const auto& c : str) {
             if (c == '-') {
               if (curr != li.begin()) {
                   curr--;
                   curr = li.erase(curr);
+                  // std::list의 `.erase()` 메소드는 파라미터로 받은 `이터레이터`가 가리키는 값을 삭제한 다음 그 다음 인덱스를 가리키는 이터레이터를 반환!
+                  // `li.erase(curr);` 처럼만 쓰게 되면 curr이 가리키는 값이 유효하지 않게 된다!!!
                 }
             } else if (c == '<') {
               if (curr != li.begin()) curr--;
@@ -30,3 +32,5 @@ int main() {
         cout << '\n';
     }
 }
+
+// `li.erase(curr++);`도 `curr = li.erase(curr);`와 똑같이 동작한다! ( 삭제를 수행한 다음, 이터레이터가 삭제된 원소 다음 번 원소를 가리키게 함으로써 이터레이터의 유효성 문제 해결 )
