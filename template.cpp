8314b5185ca5dd9b38140cc9daac894319f7bd10
성문 <castledoor@pusan.ac.kr>
2024-06-11 00:17:22 +0900

    너무 많은걸 추가하지 않도록 조심하기

--- template.cpp
+++ template.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
+
+namespace ran = std::ranges;
 using namespace std;
 using ll = long long;
 
 int main() {
-    cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+  cin.tie(nullptr)->ios_base::sync_with_stdio(false);
 
-    
+  //
 }
