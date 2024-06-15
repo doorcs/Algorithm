ef1b08289a7c7efb75169bd4ba6e1e48105978c6
성문 <castledoor@pusan.ac.kr>
2024-06-15 20:44:37 +0900

    5397번: 키로거 ***
    링크드리스트를 써야될 것 같으면 `std::list` 대신 `std::deque` 쓰기!!!!!

--- BOJ/05397.cpp
+++ BOJ/05397.cpp
@@ -1,4 +1,43 @@
 #include <bits/stdc++.h>
+
+namespace ran = std::ranges;
+using namespace std;
+using ll = long long;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+  int N; cin >> N;
+
+  while (N--) {
+    string str; cin >> str;
+    deque<char> dq;
+    auto curr = dq.begin();
+
+    for (const auto& c : str) {
+      if (c == '-') {
+        if (curr != dq.begin()) {
+          curr--;
+          curr = dq.erase(curr);
+        }
+      } else if (c == '<') {
+        if (curr != dq.begin()) curr--;
+      } else if (c == '>') {
+        if (curr != dq.end()) curr++;
+      } else {
+        curr = dq.insert(curr, c);
+        curr++;
+      }
+    }
+
+    for (const auto& c : dq) cout << c;
+    cout << '\n';
+  }
+}
+
+/* 처음 푼 코드:
+
+#include <bits/stdc++.h>
 namespace ran = std::ranges;
 using namespace std;
 using ll = long long;
@@ -34,3 +73,5 @@ int main() {
 }
 
 // `li.erase(curr++);`도 `curr = li.erase(curr);`와 똑같이 동작한다! ( 삭제를 수행한 다음, 이터레이터가 삭제된 원소 다음 번 원소를 가리키게 함으로써 이터레이터의 유효성 문제 해결 )
+
+*/
