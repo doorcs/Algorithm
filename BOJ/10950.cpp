71647c3ada65d8f72c67afe058ef587a88bea165
성문 <castledoor@pusan.ac.kr>
2024-07-21 12:08:36 +0900

    10950번: A+B - 3

---
+++ BOJ/10950.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+namespace ran = std::ranges;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int T; cin >> T;
+  while (T--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << '\n';
+  }
+}
