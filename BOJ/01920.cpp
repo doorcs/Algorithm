32c3387572951510fa898385d5efa8b73c628e24
성문 <castledoor@pusan.ac.kr>
2024-07-23 21:26:24 +0900

    1920번: 수 찾기 ****
    이진 탐색을 직접 구현하는 풀이, 이진 탐색 라이브러리를 활용한 풀이, 해시맵을 활용한 풀이

--- BOJ/01920.cpp
+++ BOJ/01920.cpp
@@ -1,3 +1,75 @@
+// 이진탐색 구현 풀이:
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+namespace ran = std::ranges;
+
+bool binary_search(const vector<int>& vec, int val);
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int N; cin >> N;
+  vector<int> v(N);
+  for (auto& it : v) cin >> it;
+  ran::sort(v);
+
+  int query; cin >> query;
+  for (int i = 0; i < query; i++) {
+    int val; cin >> val;
+    cout << (binary_search(v, val) ? 1 : 0) << '\n';
+  }
+}
+
+bool binary_search(const vector<int>& vec, int val) {
+  int fr = 0, rr = vec.size()-1;
+
+  while (fr <= rr) {
+    int mid = fr + (rr - fr) / 2;
+    if (vec[mid] == val) {
+      return true;
+    } else if (val < vec[mid]) {
+      rr = mid-1;
+    } else fr = mid+1;
+  }
+  return false;
+}
+
+/* 이진탐색 라이브러리 사용 풀이:
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+namespace ran = std::ranges;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int N; cin >> N;
+  vector<int> v(N);
+  for (auto& it : v) cin >> it;
+  ran::sort(v);
+
+  int query; cin >> query;
+  while (query) {
+    int val; cin >> val;
+    cout << (ran::binary_search(v, val) ? 1 : 0) << '\n';
+    query--;
+  }
+}
+
+
+*/
+
+
+/* 예전 풀이 (해시맵 사용)
+
 #include <bits/stdc++.h>
 namespace ran = std::ranges;
 using namespace std;
@@ -20,3 +92,5 @@ int main() {
         else cout << 0 << '\n';
     }
 }
+
+*/
