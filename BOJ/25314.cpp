3745f0589829987c43174eac48e65b704a059f3d
doorcs <castledoor@pusan.ac.kr>
2024-07-28 11:10:50 +0900

    25314번: 코딩은 체육과목 입니다

---
+++ BOJ/25314.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+namespace ran = std::ranges;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int N; cin >> N;
+  N /= 4;
+  while (N--) cout << "long ";
+  cout << "int";
+}
