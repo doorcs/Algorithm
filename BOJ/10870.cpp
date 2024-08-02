938fa9d396dfbe3d94b1eb2a22c14cd55336165d
doorcs <castledoor@pusan.ac.kr>
2024-08-02 11:13:04 +0900

    10870번: 피보나치 수 5

---
+++ BOJ/10870.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+namespace ran = std::ranges;
+
+ll dp[22] = {0, 1, 1, };
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int N; cin >> N;
+  for (int i = 3; i < N+1; i++) {
+    dp[i] = dp[i-1] + dp[i-2];
+  }
+
+  cout << dp[N];
+}
