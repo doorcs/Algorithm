7225b462a215cc2ee52b0f29f805e668a9723e18
성문 <castledoor@pusan.ac.kr>
2024-08-10 23:59:42 +0900

    11651번: 좌표 정렬하기 2 ****
    범위 기반 for문으로 입력 처리!!

--- BOJ/11651.cpp
+++ BOJ/11651.cpp
@@ -10,19 +10,25 @@ int main() {
   ios_base::sync_with_stdio(false);
 
   int N; cin >> N;
-  vector<pair<int, int>> v;
-  while (N--) {
-    int x, y; cin >> x >> y;
-    v.push_back({x, y});
-  }
+
+  vector<pair<int, int>> v(N); // range based for문과 reference로 입력을 처리하려면 벡터의 크기를 미리 설정해둬야함!
+  for (auto& [a, b] : v) cin >> a >> b; /* beautiful */
 
   ran::sort(v, [](auto a, auto b) {
     if (a.second == b.second) {
       return a.first < b.first;
     }
-
     return a.second < b.second;
   });
 
   for (const auto& it : v) cout << it.first << ' ' << it.second << '\n';
 }
+
+/* first code I wrote: ( lines 14-15 )
+  vector<pair<int, int>> v;
+  while (N--) {
+    int x, y;
+    cin >> x >> y;
+    v.push_back({x, y});
+  }
+*/
