94e3c89f44fc25d2f6530bce521c9dfab0ce483f
성문 <castledoor@pusan.ac.kr>
2024-08-18 01:23:00 +0900

    10951번: A+B - 4
    EOF

---
+++ BOJ/10951.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int A, B;
+  while (cin >> A >> B) cout << A+B << '\n';
+}
