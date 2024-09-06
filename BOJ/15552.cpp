a45b18a2ed9c90968a492440e0f3ce244a818b2f
성문 <castledoor@pusan.ac.kr>
2024-09-06 23:57:33 +0900

    15552번: 빠른 A+B

---
+++ BOJ/15552.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int T; cin >> T;
+  while (T--) {
+    int A, B;
+    cin >> A >> B;
+    cout << A+B << '\n';
+  }
+}
