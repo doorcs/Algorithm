953f6b57b275c80f8e1a3a9afbc60a6c55e0adea
성문 <castledoor@pusan.ac.kr>
2024-09-18 13:59:21 +0900

    3003번: 킹, 퀸, 룩, 비숍, 나이트, 폰 **
    for loop, reference variable
    
    + range based for loop, initializer_list ( C++11 )

--- BOJ/03003.cpp
+++ BOJ/03003.cpp
@@ -15,3 +15,25 @@ int main() {
 
   for (int i = 0; i < 6; i++) cout << comp[i]-v[i] << ' ';
 }
+
+/* 메모리를 덜 쓰는 풀이:
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  vector<int> comp{1, 1, 2, 2, 2, 8};
+
+  for (const auto& it : comp) {
+    int tmp; cin >> tmp;
+    cout << it - tmp << ' ';
+  }
+}
+
+*/
