6bc9592955c6c525831d0ee739a44ad2e4ab161e
성문 <castledoor@pusan.ac.kr>
2024-09-27 03:37:30 +0900

    template
    2024.09.27 - 아직도 프로그래머스 컴파일 옵션은 `clang++ -std=c++17`
    
    ranges 라이브러리에서
    
    ranges::sort(), ranges::unique(), ranges::fill(),
    ranges::reverse(), ranges::min(), ranges::max()
    
    정도밖에 안 쓰고 있기도 하고, ranges::views까지 공부하기에는 우선순위가 더 높은 일들이 너무나도 많다
    
    C++17을 기준으로 공부하기!

--- template.cpp
+++ template.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 
-namespace ran = std::ranges;
 using namespace std;
 using ll = long long;
 
 int main() {
-  ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
 
-  //
+  // 240927
 }
