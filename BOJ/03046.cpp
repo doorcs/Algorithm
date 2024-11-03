1f60cf7afb00021e14896bcb50d38ede97ffa7bf
성문 <castledoor@pusan.ac.kr>
2024-11-03 22:08:41 +0900

    3046번: R2
    사칙연산

---
+++ BOJ/03046.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int R1, S;
+  cin >> R1 >> S;
+
+  cout << 2*S - R1;
+}
