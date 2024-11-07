c537a418329b2928b90122dc99f0ab418e905e19
성문 <castledoor@pusan.ac.kr>
2024-11-07 23:58:34 +0900

    2420번: 사파리월드
    long long abs(long long __x);

---
+++ BOJ/02420.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int N, M;
+  cin >> N >> M;
+
+  cout << abs(N-M);
+}
