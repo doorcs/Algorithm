939b27c7709baf169e80bb1dd0687b33bf8ef058
성문 <castledoor@pusan.ac.kr>
2024-11-25 23:43:03 +0900

    5338번: 마이크로소프트 로고
    R"()"

---
+++ BOJ/5338.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  cout <<
+R"(       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|)"; // raw string literal: R"()" (C++11)
+}
