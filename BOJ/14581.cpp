520cf0425d48adbe7d08688d75e44752ab728c3b
성문 <castledoor@pusan.ac.kr>
2024-12-20 23:59:09 +0900

    14581번: 팬들에게 둘러싸인 홍준

---
+++ BOJ/14581.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  string s; cin >> s;
+  cout << ":fan::fan::fan:\n"
+    << ":fan::" << s << "::fan:\n"
+    << ":fan::fan::fan:";
+}
