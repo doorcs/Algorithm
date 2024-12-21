f1f7c8c1ced387dd9ff9ba1047f3ddd7617cd889
성문 <castledoor@pusan.ac.kr>
2024-12-21 23:57:12 +0900

    15964번: 이상한 기호
    종강 주세요

---
+++ BOJ/15964.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll A, B;
+  cin >> A >> B;
+
+  cout << (A+B)*(A-B);
+}
