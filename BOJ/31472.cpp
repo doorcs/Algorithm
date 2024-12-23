48e4ea15418b05027bf795a6d834abbb49f91cf8
성문 <castledoor@pusan.ac.kr>
2024-12-23 21:46:49 +0900

    31472번: 갈래의 색종이 자르기
    std::sqrt()의 리턴은 double형! 필요하다면 정수형으로 캐스팅해주기!

---
+++ BOJ/31472.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int W; cin >> W;
+  cout << sqrt(W*2)*4;
+}
