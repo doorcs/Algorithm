66ea5474662ee175a0a65cf07e13cb831952cbeb
성문 <castledoor@pusan.ac.kr>
2025-03-25 00:39:57 +0900

    2563번: 색종이
    이차원 벡터 vector<vector<int>> 를 활용한 풀이 추가

--- BOJ/02563.cpp
+++ BOJ/02563.cpp
@@ -1,5 +1,37 @@
 #include <bits/stdc++.h>
-namespace ran = std::ranges;
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  vector<vector<int>> brd(100, vector<int>(100)); // 전역 변수로 brd[100][100]을 잡아두는 것과 비슷
+  int N; cin >> N;
+
+  while (N--) {
+    int x, y;
+    cin >> x >> y;
+    for (int i = y; i < y + 10; i++)
+      for (int j = x; j < x + 10; j++) brd[i][j]++;
+  }
+
+  int ans = 0;
+
+  for (auto li : brd) // vector<int> li
+    for (auto elem : li) // int elem
+      if (elem) ans++; // 0만 아니라면 모두 true이므로 ++이 한번이라도 실행된 경우 true 처리
+
+  cout << ans;
+}
+
+
+/* 첫 풀이 (일반적인 이차원 배열 사용)
+
+#include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
 
@@ -28,3 +60,5 @@ int main() {
     }
     cout << ans;
 }
+
+*/
